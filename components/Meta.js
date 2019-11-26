2ed017a183c2c0bbac48d4842a4f51309869ec9c
scottie-schneider <scott@laclave.co>
2019-11-26 17:42:38 -0500

    commenting icons

--- components/Meta.js
+++ components/Meta.js
@@ -4,10 +4,10 @@ const Meta = () => (
   <Head>
     <title>Scottie Schneider - Technical Product Lead</title>
     <link rel="manifest" href="/static/manifest.json" />    
-    <link rel="icon" type="image/png" href="/static/196x196.png" sizes="196x196" />
+    {/* <link rel="icon" type="image/png" href="/static/196x196.png" sizes="196x196" />
     <link rel="icon" type="image/png" href="/static/96x96.png" sizes="96x96" />
     <link rel="icon" type="image/png" href="/static/32x32.png" sizes="32x32" />
-    <link rel="icon" type="image/png" href="/static/16x16.png" sizes="16x16" />
+    <link rel="icon" type="image/png" href="/static/16x16.png" sizes="16x16" /> */}
     
     <link href="https://fonts.googleapis.com/css?family=Fira+Sans&display=swap" rel="stylesheet"></link>
     <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />

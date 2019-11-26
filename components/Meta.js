63c94999e994fb499abf8503ee9a0dde006dc86f
scottie-schneider <scott@laclave.co>
2019-11-26 17:14:43 -0500

    changing meta

--- components/Meta.js
+++ components/Meta.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 
 const Meta = () => (
   <Head>
-    {/* <title>Scottie Schneider - Technical Product Lead</title>
+    <title>Scottie Schneider - Technical Product Lead</title>
     <link rel="manifest" href="/static/manifest.json" />    
     <link rel="icon" type="image/png" href="/static/196x196.png" sizes="196x196" />
     <link rel="icon" type="image/png" href="/static/96x96.png" sizes="96x96" />
@@ -14,16 +14,6 @@ const Meta = () => (
     <meta
       name="description"
       content="Scottie Schneider"
-    /> */}
-    <title>Raices.io</title>
-    <link rel="manifest" href="/static/manifest.json" />    
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous"/>
-    <link href="https://fonts.googleapis.com/css?family=Fira+Sans&display=swap" rel="stylesheet"></link>
-    <script src="https://kit.fontawesome.com/82de0af7c5.js" crossorigin="anonymous"></script>
-    <meta name="theme-color" content="#72B340" />
-    <meta
-      name="description"
-      content="Raices.io"
     />
   </Head>
 );

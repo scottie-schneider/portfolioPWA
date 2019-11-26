e69d5dbdd1a0d0ff5e0ff7a775a2e55d72bb5879
scottie-schneider <scott@laclave.co>
2019-11-26 17:09:34 -0500

    removing wrong favicon

--- components/Meta.js
+++ components/Meta.js
@@ -4,8 +4,6 @@ const Meta = () => (
   <Head>
     <title>Scottie Schneider - Technical Product Lead</title>
     <link rel="manifest" href="/static/manifest.json" />    
-    <link rel="shortcut icon" href="/static/favicon.ico" />
-    
     <link rel="icon" type="image/png" href="/static/196x196.png" sizes="196x196" />
     <link rel="icon" type="image/png" href="/static/96x96.png" sizes="96x96" />
     <link rel="icon" type="image/png" href="/static/32x32.png" sizes="32x32" />

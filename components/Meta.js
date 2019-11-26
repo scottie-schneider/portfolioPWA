dc280e3d47bffe5bd616fe742c5a030e0f6cda85
scottie-schneider <scott@laclave.co>
2019-11-26 15:20:28 -0500

    changing favicon on manifest

--- components/Meta.js
+++ components/Meta.js
@@ -2,13 +2,13 @@ import Head from 'next/head';
 
 const Meta = () => (
   <Head>
-    <title>Raices.io</title>
+    <title>Scottie Schneider - Technical Product Lead</title>
     <link rel="manifest" href="/static/manifest.json" />    
     <link href="https://fonts.googleapis.com/css?family=Fira+Sans&display=swap" rel="stylesheet"></link>
     <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
     <meta
       name="description"
-      content="Raices.io"
+      content="Scottie Schneider - Technical Product Lead, Designer, Developer"
     />
   </Head>
 );

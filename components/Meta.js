bfd96c5546d4adcbea5a635cf5f20711922fb8ec
scottie-schneider <scott@laclave.co>
2020-01-07 13:18:03 -0500

    title

--- components/Meta.js
+++ components/Meta.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 
 const Meta = () => (
   <Head>
-    <title>NEXT JS PWA App</title>
+    <title>{'Welcome, ${guest.firstName}!'}</title>
     <link rel="manifest" href="/static/manifest.json" />    
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css" />
     <meta name="viewport" content="width=device-width, height=device-height"></meta>
